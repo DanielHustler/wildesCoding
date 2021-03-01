c9182a950048afd851e1d81dc41826b658c1b573
Daniel Hasler <daniel.hasler3141@gmail.com>
2021-03-01 08:26:01 +0100

    defined new function for visualising correlations with significance.

--- Code/functions.R
+++ Code/functions.R
@@ -115,3 +115,37 @@ imputation = function(data){
   return(data)
   
 }
+
+
+# mat : is a matrix of data
+# ... : further arguments to pass to the native R cor.test function
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
+
+replaceNAinColumns = function(data,columns,nameOfColumns) {
+  
+  for (i in columns){
+    
+    if (sapply(data,class)[[i]] == "character"){
+      
+      data[is.na(get(i)) , (i):=paste0("No ",nameOfColumns)]
+      
+    }else{
+      
+      data[is.na(get(i)) , (i):= 0]
+      
+    }
+  }
+}
