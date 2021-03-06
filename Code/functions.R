d9907b03dde17e3fd1ec17d3359ca10a17c7d2b7
Daniel Hasler <daniel.hasler3141@gmail.com>
2021-03-06 15:02:53 +0100

    Addition of new functions 'preprocess'

--- Code/functions.R
+++ Code/functions.R
@@ -100,14 +100,6 @@ missingValues <- function(data){
 
 replaceNA = function(data) {
   
-  for (i in col_Missing_ge_10_pct){
-    
-    if (sapply(data,class)[[i]] == "character"){
-      
-      data[is.na(get(i)) , (i):=paste0(i,"_missingValue")]
-      
-    }
-  }
 }
 
 imputation = function(data){
@@ -148,4 +140,74 @@ replaceNAinColumns = function(data,columns,nameOfColumns) {
       
     }
   }
+}
+
+preProcess <- function(data){
+  
+  ## get columns of type character with more than 10% missing data. 
+  ## In those cases create new category "ValueMissing"
+  
+  missing.values.percentage <- as.data.table(missinValuesPercentage(data))
+  
+  col_Missing_ge_10_pct <- missing.values.percentage[which(missing.values.percentage$pct >= 10 & missing.values.percentage$isna == T),]$key
+  
+  for (i in col_Missing_ge_10_pct){
+    
+    if (sapply(data,class)[[i]] == "character"){
+      
+      data[is.na(get(i)) , (i):=paste0(i,"_missingValue")]
+      
+    }
+  }
+  
+  ## get columns with missing values of type character and integer separately to prepare imputation
+  
+  data_classes <- sapply(data,class)
+  
+  col_replaced <- col_Missing_ge_10_pct[which(data_classes[col_Missing_ge_10_pct] == "character")]
+  col_to_be_replaced <- setdiff(missing.values.percentage$key[which(missing.values.percentage$isna == TRUE)], col_replaced)
+  
+  col_char <- unique(names(data)[which(names(data) %in% names(data)[sapply(data,is.character)])])
+  col_int <- setdiff(names(data), col_char)
+  
+  ## visualize patterns in missing data 
+  
+  data[,..col_to_be_replaced] %>% as_shadow_upset() %>% upset(,nset = length(names(data[,..col_to_be_replaced])))
+  
+  ## After taking a look at the missing values it was decided to change all NA in coulmns concerning
+  ## garages, basements and Masonary Veneeers to "No garage", "No basement" and "No masonary veneer". 
+  ## All values of type integer will be set to 0, icluding "LotFrontage".
+  
+  col_garage <- c("GarageCond","GarageFinish","GarageQual","GarageType","GarageYrBlt")
+  col_bsmt<- c("BsmtCond","BsmtFinType1","BsmtQual","BsmtExposure","BsmtFinType2")
+  col_masVnr <- c("MasVnrArea","MasVnrType")
+  col_electrical <- c("Electrical")
+  col_LotFrontage <- c("LotFrontage")
+  
+  replaceNAinColumns(data,col_garage,"garage")
+  replaceNAinColumns(data,col_bsmt,"basement")
+  replaceNAinColumns(data,col_masVnr,"masonaryVeneer")
+  replaceNAinColumns(data,col_electrical,"electrical")
+  replaceNAinColumns(data,col_LotFrontage,"frontage")
+  
+  data <- data[complete.cases(data),]
+  
+  data_char <- data[,..col_char]
+  data_int <- data[,..col_int]
+  
+  
+  #one-hot encode all categorial variables
+  
+  dmy <- dummyVars(" ~ .", data = data_char)
+  trsf <- data.frame(predict(dmy, newdata = data_char))
+  data <- cbind(data_int, trsf)
+  
+  return(data)
+}
+
+includeSaleBuckets <- function(data, numberOfClasses){
+  
+  width_of_buckets <- (max(data[, SalePrice]) - min(data[, SalePrice])) / nrow(data)
+  data <- data[, SalePriceBucket := width_of_buckets*round(SalePrice / width_of_buckets, digits = 0 )]
+  
 }
