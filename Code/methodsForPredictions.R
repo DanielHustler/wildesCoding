57903695a67d4bf684441d38df52006fc7afa2d8
Daniel Hasler <daniel.hasler3141@gmail.com>
2021-03-12 08:39:53 +0100

    included a plot of the tree and added input variables to the frunction "pcaDecisionTree"

--- Code/methodsForPredictions.R
+++ Code/methodsForPredictions.R
@@ -1,4 +1,4 @@
-pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, salePrices_test){
+pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, salePrices_test, method, depth){
  
   #add a training set with principal components
   train.data <- data.frame(SalePrice = SalePrice$SalePriceIndex_train, data_train.pca$x)
@@ -8,8 +8,9 @@ pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, saleP
   
   #run a decision tree
   library(rpart)
-  rpart.model <- rpart(SalePrice ~ .,data = train.data, method = "anova")
-  rpart.model
+  library(rpart.plot)
+  rpart.model <- rpart(SalePrice ~ .,data = train.data, method = method, control = rpart.control(cp = depth))
+  rpart.plot(rpart.model,box.palette="blue")
   
   #transform test into PCA
   test.data <- predict(data_train.pca, newdata = as.data.table(data_test))
@@ -19,13 +20,14 @@ pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, saleP
   test.data <- test.data[,1:155]
   
   #make prediction on test data
-  rpart.prediction <-round( predict(rpart.model, test.data), digits = 0)
+  rpart.prediction <-round( predict(rpart.model, test.data, type = ), digits = 0)
   
   test_pred <- cbind(data.table(rpart.prediction), salePrices_test)
   
   salePricePredictionSpace <-unique(salePrices_train[, SalePrice_Prediction := mean(SalePrice_train), by = "SalePriceIndex_train"][,.(SalePriceIndex_train, SalePrice_Prediction)])
   
   test_pred <- merge(test_pred, salePricePredictionSpace, by.x = "rpart.prediction", by.y = "SalePriceIndex_train", all.x = TRUE)
+  test_pred<-test_pred[order(Id),]
   test_pred$sq_error = (test_pred$SalePrice_test - test_pred$SalePrice_Prediction)^2
   mse <- sum(test_pred[which(is.na(SalePrice_Prediction)!=TRUE), sq_error])
   result <- list(prediction = test_pred, mse = mse)
