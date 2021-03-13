5d828408259a1196c479c3a02d7a6eba65ace1ff
Daniel Hasler <daniel.hasler3141@gmail.com>
2021-03-13 18:24:54 +0100

    created new function for random forest model

--- Code/methodsForPredictions.R
+++ Code/methodsForPredictions.R
@@ -17,7 +17,7 @@ pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, saleP
   test.data <- as.data.frame(test.data)
   
   #select the first 155 components
-  test.data <- test.data[,1:155]
+  test.data <- test.data[,1:finalPC]
   
   #make prediction on test data
   rpart.prediction <-round( predict(rpart.model, test.data, type = ), digits = 0)
@@ -33,3 +33,51 @@ pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, saleP
   result <- list(prediction = test_pred, mse = mse)
   return(result)
 }
+
+
+
+pcaRandomForest <- function(SalePrice, 
+                            Bucket, 
+                            data_train.pca, 
+                            finalPC, 
+                            data_test,
+                            salePrices_test,
+                            numberOfTrees,
+                            numberOfFeaturesUsedInTreeConstruction,
+                            importanceOfVariablesCalculated){
+  
+  
+  
+  #add a training set with principal components
+  train.data <- data.frame(SalePrice = SalePrice$SalePriceIndex_train, data_train.pca$x)
+  
+  #we are interested in first 155 PCAs
+  train.data <- train.data[,1:(finalPC+1)]
+  
+  library(randomForest)
+  
+  randomForest.model <- randomForest(formula = SalePrice ~ .,data = train.data, importance = importanceOfVariablesCalculated, ntree = numberOfTrees, mtry = numberOfFeaturesUsedInTreeConstruction)
+
+  #transform test into PCA
+  test.data <- predict(data_train.pca, newdata = as.data.table(data_test))
+  test.data <- as.data.frame(test.data)
+  
+  #select the first 155 components
+  test.data <- test.data[,1:finalPC]
+  
+  #make prediction on test data
+  randomForest.prediction <-round( predict(randomForest.model, test.data, type = "response"), digits = 0)
+  
+  test_pred <- cbind(data.table(randomForest.prediction), salePrices_test)
+  
+  salePricePredictionSpace <-unique(salePrices_train[, SalePrice_Prediction := mean(SalePrice_train), by = "SalePriceIndex_train"][,.(SalePriceIndex_train, SalePrice_Prediction)])
+  
+  test_pred <- merge(test_pred, salePricePredictionSpace, by.x = "randomForest.prediction", by.y = "SalePriceIndex_train", all.x = TRUE)
+  test_pred<-test_pred[order(Id),]
+  test_pred$sq_error = (test_pred$SalePrice_test - test_pred$SalePrice_Prediction)^2
+  mse <- sum(test_pred[which(is.na(SalePrice_Prediction)!=TRUE), sq_error])
+  result <- list(prediction = test_pred, mse = mse)
+  
+  return(result)
+  
+  }
