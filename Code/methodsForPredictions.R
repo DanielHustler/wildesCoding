e085264e50f97d3019d983f2f328d3cebd66a22a
Daniel Hasler <daniel.hasler3141@gmail.com>
2021-03-15 20:34:57 +0100

    Included boolean for buckets

--- Code/methodsForPredictions.R
+++ Code/methodsForPredictions.R
@@ -1,7 +1,13 @@
-pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, salePrices_test, method, depth){
+pcaDecisionTree <- function(SalePrice, Bucket, data_train.pca, finalPC, data_test, salePrices_test, method, depth){
  
+  if (Bucket == TRUE){
+    salePricesForPrediction = SalePrice$SalePriceIndex_train
+  }else{
+    salePricesForPrediction = SalePrice$SalePrice_train
+  }
+  
   #add a training set with principal components
-  train.data <- data.frame(SalePrice = SalePrice$SalePriceIndex_train, data_train.pca$x)
+  train.data <- data.frame(SalePrice = salePricesForPrediction, data_train.pca$x)
   
   #we are interested in first 155 PCAs
   train.data <- train.data[,1:(finalPC+1)]
@@ -23,13 +29,22 @@ pcaDecisionTree <- function(SalePrice, data_train.pca, finalPC, data_test, saleP
   rpart.prediction <-round( predict(rpart.model, test.data, type = ), digits = 0)
   
   test_pred <- cbind(data.table(rpart.prediction), salePrices_test)
-  
-  salePricePredictionSpace <-unique(salePrices_train[, SalePrice_Prediction := mean(SalePrice_train), by = "SalePriceIndex_train"][,.(SalePriceIndex_train, SalePrice_Prediction)])
-  
-  test_pred <- merge(test_pred, salePricePredictionSpace, by.x = "rpart.prediction", by.y = "SalePriceIndex_train", all.x = TRUE)
-  test_pred<-test_pred[order(Id),]
-  test_pred$sq_error = (test_pred$SalePrice_test - test_pred$SalePrice_Prediction)^2
-  mse <- sum(test_pred[which(is.na(SalePrice_Prediction)!=TRUE), sq_error])
+  if(Bucket == TRUE){
+    
+    
+    salePricePredictionSpace <-unique(salePrices_train[, SalePrice_Prediction := mean(SalePrice_train), by = "SalePriceIndex_train"][,.(SalePriceIndex_train, SalePrice_Prediction)])
+    
+    test_pred <- merge(test_pred, salePricePredictionSpace, by.x = "rpart.prediction", by.y = "SalePriceIndex_train", all.x = TRUE)
+    test_pred<-test_pred[order(Id),]
+    test_pred$sq_error = (test_pred$SalePrice_test - test_pred$SalePrice_Prediction)^2
+    mse <- sum(test_pred[which(is.na(SalePrice_Prediction)!=TRUE), sq_error])
+    
+  }else{
+    
+    test_pred$sq_error = (test_pred$SalePrice_test - test_pred$randomForest.prediction)^2
+    mse <- sum(test_pred[which(is.na(randomForest.prediction)!=TRUE), sq_error])
+    
+  }
   result <- list(prediction = test_pred, mse = mse)
   return(result)
 }
@@ -46,10 +61,14 @@ pcaRandomForest <- function(SalePrice,
                             numberOfFeaturesUsedInTreeConstruction,
                             importanceOfVariablesCalculated){
   
-  
+  if (Bucket == TRUE){
+    salePricesForPrediction = SalePrice$SalePriceIndex_train
+  }else{
+    salePricesForPrediction = SalePrice$SalePrice_train
+  }
   
   #add a training set with principal components
-  train.data <- data.frame(SalePrice = SalePrice$SalePriceIndex_train, data_train.pca$x)
+  train.data <- data.frame(SalePrice = salePricesForPrediction, data_train.pca$x)
   
   #we are interested in first 155 PCAs
   train.data <- train.data[,1:(finalPC+1)]
@@ -70,12 +89,26 @@ pcaRandomForest <- function(SalePrice,
   
   test_pred <- cbind(data.table(randomForest.prediction), salePrices_test)
   
+  if(Bucket == TRUE){
+    
+  
   salePricePredictionSpace <-unique(salePrices_train[, SalePrice_Prediction := mean(SalePrice_train), by = "SalePriceIndex_train"][,.(SalePriceIndex_train, SalePrice_Prediction)])
   
   test_pred <- merge(test_pred, salePricePredictionSpace, by.x = "randomForest.prediction", by.y = "SalePriceIndex_train", all.x = TRUE)
   test_pred<-test_pred[order(Id),]
   test_pred$sq_error = (test_pred$SalePrice_test - test_pred$SalePrice_Prediction)^2
   mse <- sum(test_pred[which(is.na(SalePrice_Prediction)!=TRUE), sq_error])
+  
+  }else{
+    
+  test_pred$sq_error = (test_pred$SalePrice_test - test_pred$randomForest.prediction)^2
+  mse <- sum(test_pred[which(is.na(randomForest.prediction)!=TRUE), sq_error])
+    
+  }
+  
+  
+  
+  
   result <- list(prediction = test_pred, mse = mse)
   
   return(result)
